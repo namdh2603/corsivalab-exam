d319942bb04fbee4a52df170ffacbb49c77d6df6
namdh2603 <namdh2603@gmail.com>
2022-02-26 13:31:58 +0700

    remove font folder

--- wp-content/themes/demo/archive.php
+++ wp-content/themes/demo/archive.php
@@ -1,37 +1,20 @@
 <?php
-/**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
- */
-
 get_header();
-
 $description = get_the_archive_description();
 ?>
-
 <?php if ( have_posts() ) : ?>
-
-	<header class="page-header alignwide">
-		<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
-		<?php if ( $description ) : ?>
-			<div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
-		<?php endif; ?>
-	</header><!-- .page-header -->
-
-
-	<?php while ( have_posts() ) : ?>
-		<?php the_post(); ?>
-		<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
-	<?php endwhile; ?>
-
-
+<header class="page-header alignwide">
+    <?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+    <?php if ( $description ) : ?>
+    <div class="archive-description"><?php echo wp_kses_post( wpautop( $description ) ); ?></div>
+    <?php endif; ?>
+</header><!-- .page-header -->
+<?php while ( have_posts() ) : ?>
+<?php the_post(); ?>
+<?php get_template_part( 'template-parts/content/content', get_theme_mod( 'display_excerpt_or_full_post', 'excerpt' ) ); ?>
+<?php endwhile; ?>
 <?php else : ?>
-	<?php get_template_part( 'template-parts/content/content-none' ); ?>
+<?php get_template_part( 'template-parts/content/content-none' ); ?>
 <?php endif; ?>
 <div class="default-section feature-list-section" style="background-color: #F3F8EF">
     <div class="container">
@@ -42,7 +25,8 @@ $description = get_the_archive_description();
                         <div class="row align-items-center">
                             <div class="col-4">
                                 <div class="feature-icon">
-                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/11/organic-boxes.svg" alt="" title="organic-boxes">
+                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/11/organic-boxes.svg"
+                                        alt="" title="organic-boxes">
                                 </div>
                             </div>
                             <div class="col-8">
@@ -59,7 +43,8 @@ $description = get_the_archive_description();
                         <div class="row align-items-center">
                             <div class="col-4">
                                 <div class="feature-icon">
-                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/11/corporate.svg" alt="" title="corporate">
+                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/11/corporate.svg"
+                                        alt="" title="corporate">
                                 </div>
                             </div>
                             <div class="col-8">
@@ -76,7 +61,8 @@ $description = get_the_archive_description();
                         <div class="row align-items-center">
                             <div class="col-4">
                                 <div class="feature-icon">
-                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/11/delivery.svg" alt="" title="delivery">
+                                    <img src="<?php echo get_home_url(); ?>/wp-content/uploads/2021/11/delivery.svg"
+                                        alt="" title="delivery">
                                 </div>
                             </div>
                             <div class="col-8">
