6f5be26fd2610f960291f069953e4b479f4e57c5
Hai Nam Dao <namdh2603@gmail.com>
2022-04-19 13:48:08 +0700

    update

--- wp-content/themes/demo/header.php
+++ wp-content/themes/demo/header.php
@@ -21,6 +21,7 @@
     $logo = get_attachment(get_theme_mod('custom_logo'));
     $home_link = get_home_url();
     ?>
+    <!-- Mobile Menu -->
     <div class="navbar-overlay"></div>
     <div class="navbar-mobile">
         <?php
@@ -32,64 +33,63 @@
     </div>
     <!-- Header -->
     <header class="header">
-            <div class="site-header">
-                <div class="middle-header">
-                    <div class="container">
-                        <div class="row row-sm justify-content-center align-items-center">
-                            <div class="col-4 col-lg-2 text-left">
-                                <?php
-                                echo '<a href="' . $home_link . '"><img class="logo" src="' . $logo['src'] . '" alt="' . $logo['alt'] . '" /></a>';
-                                ?>
-                            </div>
-                            <div class="col-12 col-lg-8 position-static d-none d-sm-block">
-                                <div class="row align-items-center">
-                                    <div class="col-7 position-static">
-                                        <?php wp_nav_menu(
-                                            array(
-                                                'theme_location' => 'main-menu',
-                                                'container' => false,
-                                                //'container_class' => 'menu',
-                                                'menu_class' => 'navbar',
-                                                'walker' => new Default_Walker(),
-                                            )
-                                        );
-                                        ?>
-                                    </div>
-                                    <div class="col-5">
-                                        <?php echo do_shortcode('[fibosearch]'); ?>
-                                    </div>
+        <div class="site-header">
+            <div class="middle-header">
+                <div class="container">
+                    <div class="row row-sm justify-content-center align-items-center">
+                        <div class="col-4 col-lg-2 text-left">
+                            <?php
+                            echo '<a href="' . $home_link . '"><img class="logo" src="' . $logo['src'] . '" alt="' . $logo['alt'] . '" /></a>';
+                            ?>
+                        </div>
+                        <div class="col-12 col-lg-8 position-static d-none d-sm-block">
+                            <div class="row align-items-center">
+                                <div class="col-7 position-static">
+                                    <?php wp_nav_menu(
+                                        array(
+                                            'theme_location' => 'main-menu',
+                                            'container' => false,
+                                            //'container_class' => 'menu',
+                                            'menu_class' => 'navbar',
+                                            'walker' => new Default_Walker(),
+                                        )
+                                    );
+                                    ?>
+                                </div>
+                                <div class="col-5">
+                                    <?php echo do_shortcode('[fibosearch]'); ?>
                                 </div>
                             </div>
-                            <div class="col-8 col-lg-2">
-                                <div class="d-flex justify-content-end">
-                                    <div class="social-icon">
-                                        <?php if (!empty($social_list)) :
-                                            echo '<ul class="social-list">';
-                                            foreach ($social_list as $item) {
-                                                $icon_arr = get_attachment($item['icon']);
-                                                echo '<li><a href="' . $item['link_icon'] . '"><img src="' . $icon_arr['src'] . '" alt="' . $icon_arr['alt'] . '"></a></li>';
-                                            }
-                                            echo '</ul>';
-                                        endif;
-                                        ?>
+                        </div>
+                        <div class="col-8 col-lg-2">
+                            <div class="d-flex justify-content-end">
+                                <div class="social-icon">
+                                    <?php if (!empty($social_list)) :
+                                        echo '<ul class="social-list">';
+                                        foreach ($social_list as $item) {
+                                            $icon_arr = get_attachment($item['icon']);
+                                            echo '<li><a href="' . $item['link_icon'] . '"><img src="' . $icon_arr['src'] . '" alt="' . $icon_arr['alt'] . '"></a></li>';
+                                        }
+                                        echo '</ul>';
+                                    endif;
+                                    ?>
+                                </div>
+                                <div class="cart-icon header-icon">
+                                    <a href="<?php echo $cart_link; ?>">
+                                        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/shopping-bag.svg" />
+                                    </a>
+                                    <div class="cart-count">
+                                        <?php echo $cart_count; ?>
                                     </div>
-                                    <div class="cart-icon header-icon">
-                                        <a href="<?php echo $cart_link; ?>">
-                                            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/icons/shopping-bag.svg" />
-                                        </a>
-                                        <div class="cart-count">
-                                            <?php echo $cart_count; ?>
-                                        </div>
-                                    </div>
-                                    <div class="cart-total">
-                                        <?php echo $cart_total; ?>
-                                    </div>
-                                    <div class="mobile d-block d-lg-none">
-                                        <div class="navbar-toggle">
-                                            <span class="icon-bar"></span>
-                                            <span class="icon-bar"></span>
-                                            <span class="icon-bar"></span>
-                                        </div>
+                                </div>
+                                <div class="cart-total">
+                                    <?php echo $cart_total; ?>
+                                </div>
+                                <div class="mobile d-block d-lg-none">
+                                    <div class="navbar-toggle">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
                                     </div>
                                 </div>
                             </div>
@@ -97,4 +97,5 @@
                     </div>
                 </div>
             </div>
-        </header>
+        </div>
+    </header>
