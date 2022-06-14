e7811167f6821700a810d37bf989d2cbd0f4e24e
Hai Nam Dao <namdh2603@gmail.com>
2022-06-14 16:37:26 +0700

    zxc

--- wp-content/themes/demo/inc/corsivalab-register-post.php
+++ wp-content/themes/demo/inc/corsivalab-register-post.php
@@ -1,32 +1,7 @@
 <?php
-// $new_post = tr_post_type('Award', 'Awards'); //menu show option right
-// $new_post->setId('award');
-// $new_post->setTitlePlaceholder('Enter award name here...');
-// //$new_post->setIcon('book');
-// //$new_post->setArchivePostsPerPage(5);
-// $new_post->setSupports(['title', 'editor', 'thumbnail','author']);
-
-
-$argsSupport = array( 'title','thumbnail','editor','excerpt');
-$portfolio = tr_post_type('Portfolios', 'Portfolio');
-$portfolio->setSupports($argsSupport);
-$portfolio->setTitlePlaceholder('Enter title in here');
-$portfolio->setRest();
-$portfolio->setArchivePostsPerPage(-1);
-
-$categories = tr_taxonomy('Category Portfolio', 'Categories Portfolio');
-$categories->setHierarchical();
-
-
-$portfolio->apply($categories);
-
-
-tr_meta_box('Portfolio Infomation')->apply($portfolio)->setCallback(function () {
-    $form = tr_form();
-    echo $form->gallery('Gallery');
-    // echo $form->repeater('Project Scope')->setFields([
-    //     $form->text('Title'),
-    // ]);
-    // echo $form->textarea('Video')->setLabel('Link Video');
-
-});
+$new_post = tr_post_type('Award', 'Awards'); //menu show option right
+$new_post->setId('award');
+$new_post->setTitlePlaceholder('Enter award name here...');
+//$new_post->setIcon('book');
+//$new_post->setArchivePostsPerPage(5);
+$new_post->setSupports(['title', 'editor', 'thumbnail','author']);
